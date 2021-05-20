e059831ef7173a3960e9780dd36b1965c30bf549
designaky <designaky@gmail.com>
2021-05-20 15:40:57 +0100

    Adding the product page local storage
    Co-authored by: Maryam Ghorbani <36102515+maryamgh1988@users.noreply.github.com>

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,4 +1,5 @@
 import Layout from "../../components/layout";
+import React from "react"
 import Head from "next/head";
 import { getProductData, getAllProducts } from "../../database/model";
 import Image from "next/image";
@@ -8,7 +9,6 @@ export async function getStaticPaths() {
   const pathData = await getAllProducts();
 
   const paths = pathData.map(({ id }) => {
-    console.log(id);
     return {
       params: {
         id: id.toString(),
@@ -32,6 +32,39 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ productData }) {
   const productObject = JSON.parse(productData);
+  const [currentBasket, setCurrentBasket] = React.useState([]);
+
+  function addToBasket(event) {
+    event.preventDefault();
+    const findId = currentBasket.find(product=> product.id === productObject.id )
+
+    if (findId){
+      findId.quantity = findId.quantity + parseInt(event.target[0].value);
+      findId.totalPrice = findId.quantity  * productObject.price;
+      currentBasket.map(product=> product.id === productObject.id )      
+    } else {
+      const basketObj = {};
+      basketObj.id = productObject.id; 
+      basketObj.name = productObject.product_name;
+      basketObj.quantity = parseInt(event.target[0].value);
+      basketObj.colour = event.target[1].value;
+      basketObj.totalPrice = parseInt(event.target[0].value) * productObject.price;
+      currentBasket.push(basketObj);
+    }
+
+    localStorage.setItem("basket", JSON.stringify(currentBasket));
+
+  }
+
+
+  React.useEffect(()=>{
+   if(localStorage.getItem("basket")){
+   setCurrentBasket([...JSON.parse(localStorage.getItem("basket"))])
+   }
+
+  },[])
+
+
   return (
     <Layout>
       <Head>
@@ -40,13 +73,14 @@ export default function Post({ productData }) {
       <Image src={productObject.img_path} width={200} height={200}></Image>
       <section>
         <h1>{productObject.product_name}</h1>
+        <h2>£{productObject.price}</h2>
         <p>{productObject.product_description}</p>
         <p>{productObject.product_price}</p>
       </section>
 
-      <form>
+      <form onSubmit={addToBasket}>
         <label htmlFor="quantity">Quantity</label>
-        <input type="number" value="quantity" name="quantity" id="quantity" />
+        <input type="number" name="quantity" id="quantity" min={1} max={productObject.product_custom.quantity} required />
         <label htmlFor="colour">Choose a colour:</label>
         <select name="colour" id="colour">
           {productObject.product_custom.colour.map((colour) => {
