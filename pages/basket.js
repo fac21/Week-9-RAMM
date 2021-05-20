da45733f2c55dfb7fd1bbca37764dd82db5096a2
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-05-20 16:00:58 +0100

    getting basket items from local storage
    Co-authored by: Michael Devlin <23028288+michael-devlin@users.noreply.github.com>

---
+++ pages/basket.js
@@ -0,0 +1,23 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ShoppingBasket from "../components/ShoppingBasket"
+
+export default function Basket() {
+  return (
+    <Layout>
+      <Head>
+        <title>Shopping Basket</title>
+        <meta name="description" content="E-commerce site for mugs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main_container}>
+        <main className={styles.flex_column}>
+          <p>shopping basket</p>
+          <ShoppingBasket />
+        </main>
+      </div>
+    </Layout>
+  );
+}
