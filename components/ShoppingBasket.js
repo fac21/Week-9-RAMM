19b38d48e0af9bf427e79c67427ec85e4279bc3a
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-05-20 17:37:39 +0100

    adding a itemAddedMessage component and adding items to shopping basket
    Co-authored by: designaky 63958540+designaky@users.noreply.github.com

--- components/ShoppingBasket.js
+++ components/ShoppingBasket.js
@@ -1,23 +1,61 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
+
+
 
 
 export default function ShoppingBasket() {
+  const [basket, setBasket] = useState([]);
+  const [totalPrice, setTotalPrice] = useState()
 
-const [basket, setBasket] = useState([])
 
-useEffect(() => {
-    if (typeof window !== "undefined") {
-        console.log(localStorage.getItem('basket'))
-        setBasket(JSON.parse(localStorage.getItem('basket')))
 
+  // function getTotalPrice(){
+  //   let totprice = 0
+  //   const newArrey = [...basket]
+  //   newArrey.forEach((product) => {
+  //     totprice+=product.totalPrice
+  //     console.log("total",totprice)
+  //   })
+
+  //   return totprice
+    
+  // }
+
+ 
+  useEffect(() => {
+    if (localStorage.getItem("basket")) {
+      setBasket(JSON.parse(localStorage.getItem("basket")));
+
+
+    
+    } else {
+      // console.log("no local storage");
     }
+    // setTotalPrice(getTotalPrice())
     
-    
-},[])
 
-return(
+  }, []);
 
-<p>{basket.item}</p>
+  
 
-)
+  return (
+    <>
+      <ul>
+        {basket.map((e) => {
+          return (
+            <li key={e.id}>
+              <Image src={e.img_path} width={50} height={50}/>
+              <p>{e.name}</p>
+              <p>Quantity: {e.quantity}</p>
+              <p>Price: £{e.totalPrice}</p>  
+            </li>
+          );
+        })}
+      </ul>
+      {/* <>
+      {totalPrice > 0 ? <h3>Total price: £ {totalPrice}</h3> : <h3>Total price: £0</h3>}
+      </> */}
+    </>
+  );
 }
